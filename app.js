6bf038e891cd1adacc94ee76b5698423c8aa606b
PanumartFirm <pspisozsw2@hotmail.com>
2019-08-17 11:26:32 +0700

    Update pokemon

--- app.js
+++ app.js
@@ -7,19 +7,87 @@ app.get('/', (req, res) => res.send('Hello World!!!'))
 
 // GET /pokemons -> list all pokemons http://localhost:3000/pokemon
 
-let poke = [{ name: 'Mega Charizard X', type: "Fire" }
-    , { name: 'Ash-Greninja', type: "water" },
-    , { name: 'Sceptile', type: "Grass" }
-]
+
+class pokemons {
+
+    constructor(name,types,type2){
+        this.name = name
+        this.type = types
+        this.type2 = null
+        this.id = null
+    }
+    AddTyes2(type){
+        this.type2 = type
+    }
+}
+function CreatePokemon(name,type){
+    let p = new pokemons (name,type)
+    p.id = generatorNewId(poke.length)
+    return p 
+}
+
+function generatorNewId(num){
+    return  num+1
+}
+function isSufficientPameter(v){
+    return  v!== undefined || V!== null || V!== ''
+}
+
+
+let poke = []
+poke.push(CreatePokemon('Mega Charizard X','Fire'))
+poke.push(CreatePokemon('Ash-Greninja','water'))
+
+// let poke = [{ name: 'Mega Charizard X', type: "Fire" }
+//     , { name: 'Ash-Greninja', type: "water" },
+//     , { name: 'Sceptile', type: "Grass" }
+// ]
 
 app.get('/pokemons', (req, res) => res.send(poke))
 
 // POST /pokemons -> add pokemon to list
 app.post('/pokemons', (req, res) => {
-         poke.push(req.body)
-         res.sendStatus(201)   
+
+        if (isSufficientPameter(req.body.name)||
+            isSufficientPameter(req.body.type)){
+                res.status(400).send({error :'Insufficien paraments: name and type are required paramenter '})
+                return
+        }
+
+        let p = new CreatePokemon(req.body.name,req.body.type)
+        poke.push(p)
+        res.sendStatus(201)   
             // console.log(req.body)
             //res.send(req.body)
 })
 
+app.get('/pokemons/:id', (req, res) => {
+    let id = req.params.id
+    let p = poke[id - 1]
+    res.send(p)
+})
+app.put('/pokemons/:id', (req, res) => {
+    
+    if (!isSufficientPameter(req.body.type2)){
+        res.status(400).send({error :'Insufficien paraments: name and type are required paramenter '})
+        return
+    }
+    if (!isSufficientPameter(id)){
+        res.status(400).send({error :'Insufficien paraments: name and type are required paramenter '})
+        return
+    }
+    let id = req.params.id
+    let p = poke[id - 1]
+    if (p === undefined){
+        res.status(400).send({error :'Canot update pokemon: Pokemon is not fount'})
+        return
+    }
+
+    p.type2 = p.body.type2
+    poke[id-1] = p
+    res.sendStatus(200)   
+})
+
 app.listen(port, () => console.log(`Pokemon API listen  on port ${port}!`))
+
+
